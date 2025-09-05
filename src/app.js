e2ec9eca2b06ee64e2ae0eeb84a059527b654411
Mykhailo Forkosh <forkos277@gmail.com>
2025-09-05 13:11:07 +0300

    solution

--- src/app.js
+++ src/app.js
@@ -1,1 +1,23 @@
+/* eslint-disable no-console */
 'use strict';
+
+const fs = require('fs');
+
+const fileForCopy = process.argv[2];
+const copyTo = process.argv[3];
+
+if (process.argv.length < 4) {
+  console.error('Must be two arguments');
+} else {
+  fs.readFile(fileForCopy, 'utf8', (err, data) => {
+    if (err) {
+      console.error(err);
+    } else {
+      fs.writeFile(copyTo, data, (error) => {
+        if (error) {
+          console.error(error);
+        }
+      });
+    }
+  });
+}
